612345d2d9d692371de366577849cc711a2e8c13
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-19 11:30:12 +0530

    com number

---
+++ .gitignore/compo.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is a composite number")
+           break
+   else:
+       print(num,"is a prime number")
+
+else:
+   print(num,"is  a composite  number")
+   print("composite number")
